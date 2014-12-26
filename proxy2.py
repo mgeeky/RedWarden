738f437a729f09a3271a21fcb6676b59a6267c0a
inaz2 <inaz2@twitter>
2014-12-26 17:19:23 +0900

    implement stub handlers and encode/decode_content_body

--- proxy2.py
+++ proxy2.py
@@ -4,9 +4,12 @@ import select
 import httplib
 import urlparse
 import threading
+import gzip
+import zlib
 from collections import OrderedDict
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
+from cStringIO import StringIO
 
 
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
@@ -15,6 +18,7 @@ class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
 
 class ProxyRequestHandler(BaseHTTPRequestHandler):
     timeout = 5
+    lock = threading.Lock()
 
     def __init__(self, *args, **kwargs):
         self.tls = threading.local()
@@ -60,18 +64,21 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
         s.close()
 
     def do_GET(self):
-        req_headers = self.headers
-        content_length = int(req_headers.get('Content-Length', 0))
+        req = self
+        content_length = int(req.headers.get('Content-Length', 0))
         req_body = self.rfile.read(content_length) if content_length else None
 
-        # TODO: add request handler here
+        req_body_modified = self.request_handler(req, req_body)
+        if req_body_modified is not None:
+            req_body = req_body_modified
+            req.headers['Content-length'] = str(len(req_body))
 
         u = urlparse.urlsplit(self.path)
         assert u.scheme == 'http'
         host, path = u.netloc, (u.path + '?' + u.query if u.query else u.path)
         if host:
-            req_headers['Host'] = host
-        req_headers = self.filter_headers(req_headers)
+            req.headers['Host'] = host
+        req_headers = self.filter_headers(req.headers)
 
         try:
             if not host in self.tls.conns:
@@ -79,7 +86,7 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
             conn = self.tls.conns[host]
             conn.request(self.command, path, req_body, dict(req_headers))
             res = conn.getresponse()
-            res_headers = res.msg
+            setattr(res, 'headers', res.msg)
             res_body = res.read()
         except Exception as e:
             self.send_response(502, 'Bad Gateway')
@@ -91,11 +98,15 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
                 del self.tls.conns[host]
             return
 
-        # TODO: add response body decoder here
+        content_encoding = res.headers.get('Content-Encoding', 'identity')
+        res_body_plain = self.decode_content_body(res_body, content_encoding)
 
-        # TODO: add response handler here
+        res_body_modified = self.response_handler(res, res_body_plain)
+        if res_body_modified is not None:
+            res_body = self.encode_content_body(res_body_modified, content_encoding)
+            res.headers['Content-Length'] = str(len(res_body))
 
-        res_headers = self.filter_headers(res_headers)
+        res_headers = self.filter_headers(res.headers)
 
         self.wfile.write("%s %d %s\r\n" % (self.protocol_version, res.status, res.reason))
         for line in res_headers.headers:
@@ -103,6 +114,9 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(res_body)
         self.wfile.flush()
+
+        with self.lock:
+            self.save_handler(req, req_body, res, res_body)
 
     do_HEAD = do_GET
     do_POST = do_GET
@@ -113,6 +127,47 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
         for k in hop_by_hop:
             del headers[k]
         return headers
+
+    def encode_content_body(self, text, encoding):
+        if encoding in ('gzip', 'x-gzip'):
+            io = StringIO()
+            with gzip.GzipFile(fileobj=io, mode='wb') as f:
+                f.write(text)
+            data = io.getvalue()
+        elif encoding == 'deflate':
+            data = zlib.compress(text)
+        elif encoding == 'identity':
+            data = text
+        else:
+            raise Exception("Unknown Content-Encoding: %s" % encoding)
+        return data
+
+    def decode_content_body(self, data, encoding):
+        if encoding in ('gzip', 'x-gzip'):
+            io = StringIO(data)
+            with gzip.GzipFile(fileobj=io) as f:
+                text = f.read()
+        elif encoding == 'deflate':
+            text = zlib.decompress(data)
+        elif encoding == 'identity':
+            text = data
+        else:
+            raise Exception("Unknown Content-Encoding: %s" % encoding)
+        return text
+
+    def request_handler(self, req, req_body):
+        pass
+
+    def response_handler(self, res, res_body):
+        pass
+
+    def save_handler(self, req, req_body, res, res_body):
+        version_table = {10: 'HTTP/1.0', 11: 'HTTP/1.1'}
+
+        print "%s %s %s" % (req.command, req.path, req.request_version)
+        print req.headers
+        print "%s %d %s" % (version_table[res.version], res.status, res.reason)
+        print res.headers
 
 
 def test(HandlerClass = ProxyRequestHandler, ServerClass = ThreadingHTTPServer, protocol="HTTP/1.1"):
